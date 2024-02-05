7c1db7fee41bfa3e0c9c480e49dd8c4119558933
yrcho <ip323@naver.com>
2024-02-05 10:36:27 +0900

    시뮬레이션 & 구현 - 청소

---
+++ src/simulation/청소.java
@@ -0,0 +1,57 @@
+package simulation;
+import java.util.*;
+
+public class 청소 {
+
+    public int[] solution(int[][] board, int k){
+        int[] answer = new int[2];
+        int[] dx = {-1, 0, 1, 0};
+        int[] dy = {0, 1, 0, -1};
+        int d = 1;
+        int sec = 0;
+        int x = 0;
+        int y = 0;
+
+        while (sec < k) {
+            sec++;
+            int nx = x + dx[d];
+            int ny = y + dy[d];
+
+            if (nx < 0 || nx >= board.length || ny < 0 || ny >= board.length || board[nx][ny] == 1) {
+                d = (d + 1) % 4;
+                continue;
+            }
+
+            x = nx;
+            y = ny;
+        }
+
+        answer[0] = x;
+        answer[1] = y;
+
+        return answer;
+    }
+
+    public static void main(String[] args){
+        청소 T = new 청소();
+        int[][] arr1 = {{0, 0, 0, 0, 0},
+                {0, 1, 1, 0, 0},
+                {0, 0, 0, 0, 0},
+                {1, 0, 1, 0, 1},
+                {0, 0, 0, 0, 0}};
+        System.out.println(Arrays.toString(T.solution(arr1, 10)));
+        int[][] arr2 = {{0, 0, 0, 1, 0, 1},
+                {0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 1},
+                {1, 1, 0, 0, 1, 0},
+                {0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0}};
+        System.out.println(Arrays.toString(T.solution(arr2, 20)));
+        int[][] arr3 = {{0, 0, 1, 0, 0},
+                {0, 1, 0, 0, 0},
+                {0, 0, 0, 0, 0},
+                {1, 0, 0, 0, 1},
+                {0, 0, 0, 0, 0}};
+        System.out.println(Arrays.toString(T.solution(arr3, 25)));
+    }
+}
